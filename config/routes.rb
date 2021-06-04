947feaf4039d7448730800df364fcdc522d4e835
hayate112 <k.hayate0112@gmail.com>
2021-06-04 17:48:31 +0000

    [Update]Usershow

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:show, :edit, :update]
 end

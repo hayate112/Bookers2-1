ce9f09c59f1152e11d4f166f7fdf3b2e80913b2f
hayate112 <k.hayate0112@gmail.com>
2021-06-04 18:03:58 +0000

    [Update]userindex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = User.find(params[:id])
+    @book = @user.book.new
+  end
+
   def show
     @user = User.find(params[:id])
+    @book = @user.book.new
   end
 
   def edit

8a93f3f5de916cafd2b529fe6323ccbc4585eff2
hayate112 <k.hayate0112@gmail.com>
2021-06-04 18:14:43 +0000

    [Update]Bookcontroller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books
 end

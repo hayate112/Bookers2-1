1ea90486d06873742c6f8e422aca9863840ef5b6
hayate112 <k.hayate0112@gmail.com>
2021-06-05 17:36:00 +0000

    [Update]Users edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3ff4a54364fc6a15dbc84c7329cc244308cf4ad20676b93d00752ef2f820bdec270d55e82bb9707dbcab0066b7ac9762561daca65c681af8b04e7e0df7a05cf2'

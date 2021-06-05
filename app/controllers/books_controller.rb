c2eee644573a3a94d736a888e94d8844733d5eca
hayate112 <k.hayate0112@gmail.com>
2021-06-05 06:13:30 +0000

    [Update]Book index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+
   end
 
   def index
+    @book_new = Book.new
     @books = Book.all
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
   end
 

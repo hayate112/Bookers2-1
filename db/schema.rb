a4ac835ccc0880f4fdd2959f74d2ce0f24d432c8
hayate112 <k.hayate0112@gmail.com>
2021-06-06 07:28:07 +0000

    [Update]投稿機能のテンプレート化

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_060209) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "opinion"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"

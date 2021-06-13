07d398ff8e123e60c078091ee490e3074d79c54e
hayate112 <k.hayate0112@gmail.com>
2021-06-13 06:08:12 +0000

    [Update]User edit 制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,4 +31,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
